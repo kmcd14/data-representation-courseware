ac75f691fe78101d1609dcc0420cacefd8c74d57
Katie Mc Donald <katieisanimdom@gmail.com>
2022-10-23 18:08:27 +0100

    uploaded loaded assignment03-cso and its outputted json file

--- assignment03-cso.py
+++ assignment03-cso.py
@@ -1,2 +1,24 @@
 # Write a program that retrieves the dataset for the "exchequer account (historical series)" 
 # from the CSO, and stores it into a file called "cso.json"
+
+#https://data.gov.ie/dataset?q=exchequer+account+%28historical+series%29&sort=score+desc%2C+metadata_created+desc
+
+# Import libaries
+import requests
+import json
+
+
+# Api link
+url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/1.0/en"
+
+
+# Function to get the data
+def getData():
+    response = requests.get(url)
+    print(response.json())
+
+
+if __name__ == "__main__":
+    # Storing in a json file.
+    with open("cso.json","wt") as fp:
+        print(json.dumps(getData()), file=fp)
